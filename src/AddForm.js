584b9e94aa21d054b40c0a8f205e0b161e75b325
lukewaring <lukewaring@gmail.com>
2020-04-11 18:52:26 -0400

    Set up eslint and prettier

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,86 +1,121 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
+/* eslint-disable react/prop-types */
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 class AddForm extends React.Component {
-    state = {
-        creditorName: '',
-        firstName: '',
-        lastName: '',
-        minPaymentPercentage: 0,
-        balance: 0
-    }
+  state = {
+    creditorName: "",
+    firstName: "",
+    lastName: "",
+    minPaymentPercentage: 0,
+    balance: 0
+  };
 
-    handleChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        
-        fetch('http://localhost:3001/debts', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
-            body: JSON.stringify({ 
-                creditorName: this.state.creditorName,
-                firstName: this.state.firstName,
-                lastName: this.state.lastName,
-                minPaymentPercentage: parseFloat(this.state.minPaymentPercentage),
-                balance: parseFloat(this.state.balance)
-            })    
-        })
-        .then(res => res.json())
-        .then(newDebt => this.props.addNewDebtToAppState(newDebt))
+  handleSubmit = e => {
+    e.preventDefault();
 
-        this.props.toggleForm()
-    }
+    fetch("http://localhost:3001/debts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        creditorName: this.state.creditorName,
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        minPaymentPercentage: parseFloat(this.state.minPaymentPercentage),
+        balance: parseFloat(this.state.balance)
+      })
+    })
+      .then(res => res.json())
+      .then(newDebt => this.props.addNewDebtToAppState(newDebt));
 
-    render() {
-        return (
-            <div style={{ textAlign: 'center' }}>
-                <h3>New Debt</h3>
-                
-                <form onSubmit={e => this.handleSubmit(e)} autoComplete="off">
-                    <TextField onChange={e => this.handleChange(e)} label="Creditor" name="creditorName" value={this.state.creditorName} required />
-                    <br></br>
-                    <br></br>
-                    <TextField onChange={e => this.handleChange(e)} label="First Name" name="firstName" value={this.state.firstName} required />
-                    <br></br>
-                    <br></br>
-                    <TextField onChange={e => this.handleChange(e)} label="Last Name" name="lastName" value={this.state.lastName} required />
-                    <br></br>
-                    <br></br>
-                    <TextField onChange={e => this.handleChange(e)} label="Min Pay %" name="minPaymentPercentage" value={this.state.minPaymentPercentage} required />
-                    <br></br>
-                    <br></br>
-                    <TextField onChange={e => this.handleChange(e)} label="Balance" name="balance" value={this.state.balance} required />
-                    <br></br>
-                    <br></br>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        size="large"
-                        style={{ margin: '1rem' }}
-                    >
-                    Save
-                    </Button>
-                    <Button
-                        variant="contained"
-                        color="secondary"
-                        size="large"
-                        onClick={this.props.toggleForm}
-                    >
-                    Cancel
-                    </Button>
-                    <br></br>
-                    <div></div>
-                    <br></br>
-                </form>
-            </div>
-        )}
-    }
+    this.props.toggleForm();
+  };
 
-export default AddForm
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <h3>New Debt</h3>
+
+        <form onSubmit={e => this.handleSubmit(e)} autoComplete="off">
+          <TextField
+            onChange={e => this.handleChange(e)}
+            label="Creditor"
+            name="creditorName"
+            value={this.state.creditorName}
+            required
+          />
+          <br></br>
+          <br></br>
+          <TextField
+            onChange={e => this.handleChange(e)}
+            label="First Name"
+            name="firstName"
+            value={this.state.firstName}
+            required
+          />
+          <br></br>
+          <br></br>
+          <TextField
+            onChange={e => this.handleChange(e)}
+            label="Last Name"
+            name="lastName"
+            value={this.state.lastName}
+            required
+          />
+          <br></br>
+          <br></br>
+          <TextField
+            onChange={e => this.handleChange(e)}
+            label="Min Pay %"
+            name="minPaymentPercentage"
+            value={this.state.minPaymentPercentage}
+            required
+          />
+          <br></br>
+          <br></br>
+          <TextField
+            onChange={e => this.handleChange(e)}
+            label="Balance"
+            name="balance"
+            value={this.state.balance}
+            required
+          />
+          <br></br>
+          <br></br>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            size="large"
+            style={{ margin: "1rem" }}
+          >
+            Save
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            onClick={this.props.toggleForm}
+          >
+            Cancel
+          </Button>
+          <br></br>
+          <div></div>
+          <br></br>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddForm;
--- src/App.js
+++ src/App.js
@@ -1,111 +1,154 @@
-import React from 'react'
-import MaterialTable from 'material-table'
-import Button from '@material-ui/core/Button'
-import AddForm from './AddForm'
+import React from "react";
+import MaterialTable from "material-table";
+import Button from "@material-ui/core/Button";
+import AddForm from "./AddForm";
 
 class App extends React.Component {
-    state = {
-      debts: [],
-      isFormVisible: false,
-      selectedRowsArr: [],
-      selectedRowsTotal: 0
-    }
-  
+  state = {
+    debts: [],
+    isFormVisible: false,
+    selectedRowsArr: [],
+    selectedRowsTotal: 0
+  };
+
   componentDidMount() {
-      fetch('http://localhost:3001/debts')
+    fetch("http://localhost:3001/debts")
       .then(res => res.json())
-      .then(json => this.setState({ debts: json }))
+      .then(json => this.setState({ debts: json }));
   }
 
   currencyFormat(num) {
-      return `${num.toFixed(2)}`;
+    return `${num.toFixed(2)}`;
   }
 
   percentFormat(num) {
-      return `${num.toFixed(2)}%`;
+    return `${num.toFixed(2)}%`;
   }
-  
+
   toggleForm = () => {
-      this.setState(prevState => ({
-          isFormVisible: !prevState.isFormVisible
-      }))
-  }
+    this.setState(prevState => ({
+      isFormVisible: !prevState.isFormVisible
+    }));
+  };
 
   removeDebt = () => {
-    let selectedRowIds = this.state.selectedRowsArr.map(row => row.id)
-    this.deleteDebt(selectedRowIds)
-  }
+    let selectedRowIds = this.state.selectedRowsArr.map(row => row.id);
+    this.deleteDebt(selectedRowIds);
+  };
 
-  deleteDebt(selectedRowIds, filteredDebts) {
-    selectedRowIds.map(id => fetch(`http://localhost:3001/debts/${id}`, {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'}
-    })
-        .then(res => res.json())
-    )
-    window.location.reload(false)
+  deleteDebt(selectedRowIds) {
+    selectedRowIds.map(id =>
+      fetch(`http://localhost:3001/debts/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        }
+      }).then(res => res.json())
+    );
+    window.location.reload(false);
   }
 
   calculateTotal(rows) {
-      let total = 0
-      rows.map(row => total += row.balance )
-      this.setState({ selectedRowsTotal: total })
+    let total = 0;
+    rows.map(row => (total += row.balance));
+    this.setState({ selectedRowsTotal: total });
   }
 
   handleStateUpdates(rows) {
-      this.setState({ selectedRowsArr: rows })
-      this.calculateTotal(rows)
+    this.setState({ selectedRowsArr: rows });
+    this.calculateTotal(rows);
   }
 
-  addNewDebtToAppState = (newDebt) => {
-    this.setState({ debts: [...this.state.debts, newDebt]})
-  }
+  addNewDebtToAppState = newDebt => {
+    this.setState({ debts: [...this.state.debts, newDebt] });
+  };
 
   render() {
-      return (
-          <div style={{ padding: '2rem' }}>
-              <h2 style={{ textAlign: 'center' }}>Strategic Financial Solutions</h2>
-              <MaterialTable
-                  title=""
-                  columns={[
-                  { title: 'Creditor', field: 'creditorName', headerStyle: {fontWeight: 'bold'} },
-                  { title: 'First Name', field: 'firstName', headerStyle: {fontWeight: 'bold'} },
-                  { title: 'Last Name', field: 'lastName', headerStyle: {fontWeight: 'bold'} },
-                  { title: 'Min Pay %', field: 'minPaymentPercentage', headerStyle: {fontWeight: 'bold', textAlign: 'right'}, type: 'numeric' },
-                  { title: 'Balance', field: 'balance', headerStyle: {fontWeight: 'bold', textAlign: 'right'}, type: 'currency' },
-                  ]}
-                  data={this.state.debts}        
-                  options={{
-                  selection: true,
-                  sorting: false,
-                  search: false,
-                  paging: false,
-                  toolbar: false,
-                  }}
-                  onSelectionChange={(rows) => this.handleStateUpdates(rows) }
-              />
-              <br></br>
+    return (
+      <div style={{ padding: "2rem" }}>
+        <h2 style={{ textAlign: "center" }}>Strategic Financial Solutions</h2>
+        <MaterialTable
+          title=""
+          columns={[
+            {
+              title: "Creditor",
+              field: "creditorName",
+              headerStyle: { fontWeight: "bold" }
+            },
+            {
+              title: "First Name",
+              field: "firstName",
+              headerStyle: { fontWeight: "bold" }
+            },
+            {
+              title: "Last Name",
+              field: "lastName",
+              headerStyle: { fontWeight: "bold" }
+            },
+            {
+              title: "Min Pay %",
+              field: "minPaymentPercentage",
+              headerStyle: { fontWeight: "bold", textAlign: "right" },
+              type: "numeric"
+            },
+            {
+              title: "Balance",
+              field: "balance",
+              headerStyle: { fontWeight: "bold", textAlign: "right" },
+              type: "currency"
+            }
+          ]}
+          data={this.state.debts}
+          options={{
+            selection: true,
+            sorting: false,
+            search: false,
+            paging: false,
+            toolbar: false
+          }}
+          onSelectionChange={rows => this.handleStateUpdates(rows)}
+        />
+        <br></br>
 
-              { this.state.isFormVisible ? <AddForm toggleForm={this.toggleForm}  debts={this.state.debts} addNewDebtToAppState={this.addNewDebtToAppState} styles={"backgroundColor: 'white'"}/> :
-                  <Button onClick={this.toggleForm} variant="contained" color="primary">Add Debt</Button>
-              }
-              <br></br>
-              <br></br>
+        {this.state.isFormVisible ? (
+          <AddForm
+            toggleForm={this.toggleForm}
+            debts={this.state.debts}
+            addNewDebtToAppState={this.addNewDebtToAppState}
+            styles={"backgroundColor: 'white'"}
+          />
+        ) : (
+          <Button onClick={this.toggleForm} variant="contained" color="primary">
+            Add Debt
+          </Button>
+        )}
+        <br></br>
+        <br></br>
 
-              <Button onClick={this.removeDebt} variant="contained" color="secondary">Remove Debt(s)</Button>
-              <br></br>
-              <br></br>
+        <Button onClick={this.removeDebt} variant="contained" color="secondary">
+          Remove Debt(s)
+        </Button>
+        <br></br>
+        <br></br>
 
-              <div>
-                  <div style={{ textAlign: 'right', backgroundColor: '#a6d4fa' }}><strong>Total:  ${this.state.selectedRowsTotal.toFixed(2)}</strong></div>
-                  <br></br>
-                  <span><strong>Total Row Count: {this.state.debts.length}</strong></span>
-                  <span style={{ color: 'white' }}>a spacing div. it's a little hacky. ye</span>
-                  <span><strong>Checked Row Count: {this.state.selectedRowsArr.length}</strong></span>
-              </div>
+        <div>
+          <div style={{ textAlign: "right", backgroundColor: "#a6d4fa" }}>
+            <strong>Total: ${this.state.selectedRowsTotal.toFixed(2)}</strong>
           </div>
-      )
+          <br></br>
+          <span style={{ marginRight: "14rem" }}>
+            <strong>Total Row Count: {this.state.debts.length}</strong>
+          </span>
+          <span>
+            <strong>
+              Checked Row Count: {this.state.selectedRowsArr.length}
+            </strong>
+          </span>
+        </div>
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
